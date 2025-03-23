e3df213adca50905185cef7685ac0be7eed8ed36
kakegawa167 <kakesho167@gmail.com>
2025-03-23 13:49:34 +0900

    add README.md & fix main.py

---
+++ README.md
@@ -0,0 +1,13 @@
+## 環境変数（.env）
+
+このアプリでは以下の環境変数が必要です。
+ルートに `.env` ファイルを作成して設定してください。
+`.env.example` にサンプルを用意していますので、コピーして編集しても OK です。
+
+OPENAI_API_KEY=sk-xxx
+SLACK_BOT_TOKEN=xoxb-xxx
+SLACK_SIGNING_SECRET=xxx
+MYSQL_HOST=db
+MYSQL_USER=user
+MYSQL_PASSWORD=xxx
+MYSQL_DB=xxx
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import mysql.connector
 from slack_sdk import WebClient
 from dotenv import load_dotenv
 import time
-from flask import Flask 
+from flask import Flask
+from slack_sdk.errors import SlackApiError
 
 print("⏳ DB起動を待っています...")
 time.sleep(5)  # 5秒待つ(DBの接続待ち。必要に応じて長くする)
@@ -36,7 +37,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Flask サーバーが Docker 上で動作中です！"
+    # slackの履歴取得テスト
+    channel_id = "C0741D97FN1" 
+    
+    html = "<h2>Slackメッセージ一覧</h2><ul>"
+
+    try:
+        response = slack_client.conversations_history(channel=channel_id, limit=5)
+        messages = response["messages"]
+    
+        for msg in messages:
+            user = msg.get("user", "（不明）")
+            text = msg.get("text", "")
+            html += f"<li>{user}: {text}</li>"
+
+    except SlackApiError as e:
+        html += f"<li>Slack APIエラー: {e.response['error']}</li>"
+
+    html += "</ul>"
+
+    return html
 
 if __name__ == "__main__":
     # 本番ではdubug=Trueを外す(debug=Trueだとソースコードが表示されたり環境変数が見れたりするため)
